6e1fce0caf26214c4ca9bed39a5d3ed2e07a8fe1
Richard <richardhags@gmail.com>
2017-10-26 10:59:18 +0200

    first commit

---
+++ BranchTest/src/Klass2.java
@@ -0,0 +1,10 @@
+
+public class Klass2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Branching1");
+
+	}
+
+}
---
+++ BranchTest/src/Klass3.java
@@ -0,0 +1,9 @@
+
+public class Klass3 {
+
+	public static void main(String[] args) {
+		System.out.println("Testar branching");
+
+	}
+
+}
